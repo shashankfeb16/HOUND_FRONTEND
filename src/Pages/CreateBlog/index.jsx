2547298f31e8ff355569ddcc0850e211c5f23435
Shashank Nath <shashank.nath@yeppersindia.com>
2024-01-29 18:05:50 +0530

    Added api to create Blog

--- src/Pages/CreateBlog/index.jsx
+++ src/Pages/CreateBlog/index.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useRef, useMemo } from "react";
 import JoditEditor from "jodit-react";
 import { Box, Button, Stack, TextField } from "@mui/material";
+import axios from "axios";
 
 function CreateBlog({ placeholder }) {
   const editor = useRef(null);
   const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
   const config = useMemo(
     () => ({
       readonly: false,
@@ -16,11 +18,26 @@ function CreateBlog({ placeholder }) {
     }),
     [placeholder]
   );
-  console.log(content);
+  const handleChangeTitle = (e) =>{
+    setTitle(e.target.value);
+  }
+  const handlePublish = async() =>{
+    const data = {
+      title: title,
+      description: content
+    }
+   try{
+    await axios.post("http://localhost:8000/api/v1/blog/create", data, {withCredentials: true});
+    alert("Blog Created Successfully");
+   }
+   catch(error){
+    alert(error);
+   }
+  }
   return (
     <Box>
       <Stack spacing={2} sx={{ padding: "30px" }}>
-        <TextField label="Enter Blog Title" sx={{width: "80%"}}/>
+        <TextField label="Enter Blog Title" sx={{width: "80%"}} onChange={(e)=>handleChangeTitle(e)}/>
         <JoditEditor
           ref={editor}
           value={content}
@@ -29,7 +46,7 @@ function CreateBlog({ placeholder }) {
           onBlur={(newContent) => setContent(newContent)}
         />
         <Box sx={{display: "flex", justifyContent: "flex-end"}}>
-            <Button variant="contained">Publish</Button>
+            <Button variant="contained" onClick={handlePublish}>Publish</Button>
         </Box>
       </Stack>
     </Box>
